7bbeb57adccd1f88e94e726b8c91bf23ebfc5864
marco135 <mkuepers@gmx.de>
2015-02-18 15:17:14 +0100

    Rspec Validation-Test für Item, Preis & Amount auf 2 Decimalstellen begrenzt

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ActiveRecord::Base
   belongs_to :category
   belongs_to :article
   belongs_to :user
+  validates :name, presence: true
+  # validates :category, presence: true
+  validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }
 end
