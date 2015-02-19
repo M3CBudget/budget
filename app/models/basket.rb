3c4ab2bc89eb78e1f5eab02fcd1fec9ec7a666c3
DerNapster <christoper.werning@gmail.com>
2015-02-19 21:53:06 +0100

    Fehlerbehebung - Basket löschen, updaten von Items

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -2,8 +2,8 @@ class Basket < ActiveRecord::Base
   belongs_to :user
   belongs_to :payment
   belongs_to :vendor
-  has_many :items
-  accepts_nested_attributes_for :items, allow_destroy: true
+  has_many :items, :dependent => :destroy
+  accepts_nested_attributes_for :items, :allow_destroy => true
   validates :amount, :user_id, :vendor_id, presence: true
   validates :amount, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }
 end
