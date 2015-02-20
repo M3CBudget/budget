97090bea1bd6dfc00cb53ff0499c7f664b7841aa
DerNapster <christoper.werning@gmail.com>
2015-02-20 20:17:24 +0100

    welcome cotroller geändert, unfähig eine summe über die letzten 5 baskets zu ziehen

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
+
+    respond_to :html
+
   def index
+    @lastBaskets = Basket.last(5)
+    #@sumOfAmount = @lastBaskets.sum(:amount)
+    respond_with(@lastBaskets, @sumOfAmount )
   end
 end
