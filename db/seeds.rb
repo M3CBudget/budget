2a190156a727c78be6e65616b92c426174e3416f
DerNapster <christoper.werning@gmail.com>
2015-02-21 20:02:11 +0100

    db:seed fertig, für Datenintegrität muss die Tabellenstatistik zurückgesetzt werden

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#Vendor
 Vendor.create!([  {name: 'Aldi'},
                   {name: 'REWE'},
                   {name: 'EDEKA'},
@@ -16,15 +15,17 @@ Vendor.create!([  {name: 'Aldi'},
                   {name: 'Amazon'}
                ])
 
-
-
-# Account
 Account.create!([  {name: 'Sparkasse', number:'1634001234', bic:'26550150', active:true, user_id:1},
                    {name: 'Volksbank IBAN', number:'AL90208110080000001039531801', bic:'AL9020811008', active:true, user_id:2},
                    {name: 'Barzahlung', number:'0', bic:'0', active:true, user_id:1}
                 ])
 
-# Category
+Payment.create!([
+                    {name: 'Sparkasse', active:true, account_id:1},
+                    {name: 'Volksbank IBAN', active:true, account_id:2},
+                    {name: 'Barzahlung', active:true, account_id:3},
+                ])
+
 Category.create!([  {name: 'Haushalt', notice:'Kategorie für Haushalt', active:'true', income:'false'},
                     {name: 'Sport', notice:'Kategorie für Sport', active:'true', income:'false'},
                     {name: 'Sprit', notice:'Kategorie für Sprit', active:'true', income:'false'},
@@ -33,54 +34,34 @@ Category.create!([  {name: 'Haushalt', notice:'Kategorie für Haushalt', active:
                     {name: 'Geschenk', notice:'Einnahmen durch Geschenke', active:'true', income:'true'},
                     {name: 'Gehalt', notice:'Einnahmen durch Gehälter', active:'true', income:'true'},
                     {name: 'Gewinne', notice:'Einnahmen durch Gewinne', active:'true', income:'true'},
-                    {name: 'Sonstige', notice:'Sonstige Einnahmen', active:'true', income:'true'},
+                    {name: 'Sonstige', notice:'Sonstige Einnahmen', active:'true', income:'true'}
                 ])
 
 User.create!([    { email: '0123456@web.de', password:'12345678', first_name:'Günther', last_name:'Gurke'},
-                  { email: 'd.duese@web.de', password:'12345678', first_name:'Daniel', last_name:'Düsentrieb'},
+                  { email: 'd.duese@web.de', password:'12345678', first_name:'Daniel', last_name:'Düsentrieb'}
              ])
 
 
-# t.date :finish
-item_list_rewe = [
-    # [Name, Quantity, Price, Category]
-    ["Quark", 2, 0.59, 3],
-    ["Milch", 1, 0.79, 3],
-    ["Bananen", 5, 1.99, 3]
-]
+Item.create!([
+                 {basket_id: 1, category_id: 1, user_id: 1, name: 'Quark', quantity:5, price:0.99, notice: 'Quark für Mark', income: false, launch: Time.now.to_formatted_s(:db)},
+                 {basket_id: 1, category_id: 1, user_id: 1, name: 'Bier', quantity:1, price:4.20, notice: '', income: false, launch: Time.now.to_formatted_s(:db)},
+                 {basket_id: 1, category_id: 1, user_id: 1, name: 'Parmesan', quantity:2, price:3.99, notice: '', income: false, launch: Time.now.to_formatted_s(:db)},
+                 {basket_id: 1, category_id: 1, user_id: 1, name: 'WC Reiniger', quantity:1, price:0.79, notice: '', income: false, launch: Time.now.to_formatted_s(:db)},
+                 {basket_id: 1, category_id: 4, user_id: 1, name: 'Deo Axe', quantity:1, price:3.89, notice: 'wird auch immer teurer', income: false, launch: Time.now.to_formatted_s(:db)},
+                 {basket_id: 2, category_id: 3, user_id: 1, name: 'Super E10', quantity:42, price:1.29, notice: 'Tour Osnabrück - Münster', income: false, launch: Time.now.to_formatted_s(:db)},
+                 {basket_id: 3, category_id: 2, user_id: 1, name: 'Tischtennis Bälle', quantity:10, price:1.00, notice: '', income: false, launch: Time.now.to_formatted_s(:db)},
+                 {basket_id: 3, category_id: 2, user_id: 1, name: 'Tischtennis Schläger', quantity:2, price:34.99, notice: '', income: false, launch: Time.now.to_formatted_s(:db)},
+                 {basket_id: 4, category_id: 5, user_id: 2, name: 'Lotto', quantity:1, price:53.80, notice: 'Diesmal gewinn ich!', income: false, launch: Time.now.to_formatted_s(:db)},
+                 {category_id: 6, user_id: 1, name: 'Weihnachten Oma', quantity:1, price:50.80, notice: 'Omi ist die Beste', income: true, launch: Time.now.to_formatted_s(:db)},
+                 {category_id: 7, user_id: 1, name: 'Gehalt', quantity:1, price:2000.80, notice: 'FH Münster', income: true, launch: Time.now.to_formatted_s(:db)}
+             ])
 
-# t.string :notice
-# t.decimal :amount
-# t.binary :document
-# t.references :user, index: true
-# t.references :payment, index: true
-# t.references :vendor, index: true
+Basket.create!([
+                   {notice: '', amount: 100, vendor_id: 1, user_id: 1, payment_id: 3},
+                   {notice: '', amount: 50, vendor_id: 6, user_id: 1, payment_id: 1},
+                   {notice: '', amount: 100, vendor_id: 4, user_id: 1, payment_id: 3},
+                   {notice: '', amount: 53.80, vendor_id: 2, user_id: 2, payment_id: 2}
 
-# validates :amount, :user_id, :vendor_id, presence: true
-# validates :amount, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }
-basket_list = [
-    ["Notiz 1", 3.37, 1, 1, 1]
-]
+               ])
 
-#user_list.each do |user|
-#  User.create( :email => user[0], :encrypted_password => user[1], :first_name => user[2], :last_name => user[3]  )
-#end
-
-
-
-#vendor_list.each do |vendor|
-#  Vendor.create ( :name => vendor[0] )
-#end
-
-#account_list.each do |account|
-#  Account.create ( :number => account[0], :bic => account[1], :name => account[2], :active => account[3], :user_id => account[4] )
-#end
-
-#category_list.each do |category|
-#  Category.create ( :name => category[0], :notice => category[1] , :active => category[2], :income => category[3])
-#end
-
-#basket_list.each do |basket|
-#  Basket.create ( :notice => basket[0], :amount => basket[1] , :user => basket[2], :payment => basket[3], :vendor => basket[4])
-#end
 
