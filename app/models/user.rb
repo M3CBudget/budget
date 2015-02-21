66785a78d0d1a995467c204151b66455e34981ae
DerNapster <christoper.werning@gmail.com>
2015-02-21 09:57:00 +0100

    Commit am Morgen vertreibt kummer und sorgen, devise ist repariert!

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ActiveRecord::Base
   has_many :baskets
   has_many :accounts
   has_many :items
-  attr_accessor :login
+  attr_accessor :email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:email]
   # Virtual attribute for authenticating by either username or email
   # This is in addition to a real persisted field like 'username'
 
