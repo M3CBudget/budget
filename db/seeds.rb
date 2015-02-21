ed956ed49a371d27802398d761a6be8f40874200
mark-mit-k <warneke.mark@gmail.com>
2015-02-21 17:15:21 +0100

    seeds erweitert

--- db/seeds.rb
+++ db/seeds.rb
@@ -66,9 +66,40 @@ user_list = [
   ["markw@gmail.de", "marktest", "mark", "warne"]
 ]
 
+# t.references :basket, index: true
+# t.references :category, index: true
+# t.references :article, index: true
+# t.references :user, index: true
+# t.string :name
+# t.decimal :quantity
+# t.decimal :price
+# t.string :notice
+# t.boolean :income
+# t.integer :period
+# t.date :launch
+# t.date :finish
+item_list_rewe = [
+    # [Name, Quantity, Price, Category]
+    ["Quark", 2, 0.59, 3],
+    ["Milch", 1, 0.79, 3],
+    ["Bananen", 5, 1.99, 3]
+]
+
+# t.string :notice
+# t.decimal :amount
+# t.binary :document
+# t.references :user, index: true
+# t.references :payment, index: true
+# t.references :vendor, index: true
+
+# validates :amount, :user_id, :vendor_id, presence: true
+# validates :amount, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }
+basket_list = [
+    ["Notiz 1", 3.37, 1, 1, 1]
+]
 
 user_list.each do |user|
-  User.create ( :email => user[0], :encrypted_password => user[1], :first_name => user[3], :last_name => user[4]  )
+  User.create ( :email => user[0], :encrypted_password => user[1], :first_name => user[2], :last_name => user[3]  )
 end
 
 vendor_list.each do |vendor|
@@ -83,6 +114,6 @@ category_list.each do |category|
   Category.create ( :name => category[0], :notice => category[1] , :active => category[2], :income => category[3])
 end
 
-user_list.each do |user|
-  User.create ( :name => user[0] )
+basket_list.each do |basket|
+  Basket.create ( :notice => basket[0], :amount => basket[1] , :user => basket[2], :payment => basket[3], :vendor => basket[4])
 end
