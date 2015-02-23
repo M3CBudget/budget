2e93295468e6156cdfe50c075c4e165395fd5b70
mark-mit-k <warneke.mark@gmail.com>
2015-02-23 23:36:40 +0100

    views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   before_action :authenticate_user!
   def index
     @lastBaskets = Basket.where(:user_id => current_user.id).last(5)
-    @lastCategories = Category.where(:user_id => current_user.id).last(5)
+    @lastIncomes = Item.where(:user_id => current_user.id, :income => true).last(5)
 
     @chart = LazyHighCharts::HighChart.new('column') do |f|
       f.series(:name=>'Einnahmen', :data=> [ sum_price(find_incomes_of_user).to_i ] )
