59a3538f68c7abc997ad53fc90e310118a491a9b
DerNapster <christoper.werning@gmail.com>
2015-02-26 14:08:26 +0100

    added chart anzahl bezahlvorgänge für payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -13,7 +13,7 @@ class PaymentsController < ApplicationController
       payment_amount << sum_amount(find_baskets_of_payment(p.id))
     end
 
-    @chartPayments = LazyHighCharts::HighChart.new('pie') do |f|
+    @chartPaymentsAmount = LazyHighCharts::HighChart.new('pie') do |f|
       f.chart({:defaultSeriesType=>"pie"} )
       series = {
           :type=> 'pie',
@@ -25,7 +25,36 @@ class PaymentsController < ApplicationController
           ]
       }
       f.series(series)
-      f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'})
+      f.options[:title][:text] = 'Wertmäßiger Anteil'
+      f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> 'auto',:top=> 'auto'})
+      f.plot_options(:pie=>{
+                         :allowPointSelect=>true,
+                         :cursor=>"pointer" ,
+                         :dataLabels=>{
+                             :enabled=>true,
+                             :color=>"black",
+                             :style=>{
+                                 :font=>"13px Trebuchet MS, Verdana, sans-serif"
+                             }
+                         }
+                     })
+    end
+
+
+    @chartPaymentsCount = LazyHighCharts::HighChart.new('pie') do |f|
+      f.chart({:defaultSeriesType=>"pie"} )
+      series = {
+          :type=> 'pie',
+          :data=> [
+              [ payment_name[0], Basket.where(:user_id => current_user.id, :payment_id => Payment.where(:user_id => current_user.id, :name => payment_name[0]).pluck(:id)).count],
+              [ payment_name[1], Basket.where(:user_id => current_user.id, :payment_id => Payment.where(:user_id => current_user.id, :name => payment_name[1]).pluck(:id)).count],
+              [ payment_name[2], Basket.where(:user_id => current_user.id, :payment_id => Payment.where(:user_id => current_user.id, :name => payment_name[2]).pluck(:id)).count],
+              [ payment_name[3], Basket.where(:user_id => current_user.id, :payment_id => Payment.where(:user_id => current_user.id, :name => payment_name[3]).pluck(:id)).count]
+          ]
+      }
+      f.series(series)
+      f.options[:title][:text] = 'Anzahl der Bezahlvorgänge'
+      f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> 'auto',:top=> 'auto'})
       f.plot_options(:pie=>{
                          :allowPointSelect=>true,
                          :cursor=>"pointer" ,
